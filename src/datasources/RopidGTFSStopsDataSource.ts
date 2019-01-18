67c18ce4c72ee0a203948cbecf3935fd18afe3e3
Tomáš Benák <benak@operatorict.cz>
2019-01-18 17:06:03 +0100

    :construction: #40 RopidGTFS stazeni a transformace CIS ciselniku

---
+++ src/datasources/RopidGTFSStopsDataSource.ts
@@ -0,0 +1,94 @@
+"use strict";
+
+import * as path from "path";
+
+// import { RopidGTFSStopsDataSource as schemaObject } from "data-platform-schema-definitions";
+import CustomError from "../helpers/errors/CustomError";
+import Validator from "../helpers/Validator";
+import BaseDataSource from "./BaseDataSource";
+import IDataSource from "./IDataSource";
+import ISourceRequest from "./ISourceRequest";
+
+const config = require("../config/ConfigLoader");
+const ftp = require("basic-ftp");
+const fs = require("fs");
+
+/**
+ * TODO rozdelit na HTTP a FTP data source
+ */
+export default class RopidGTFSStopsDataSource extends BaseDataSource implements IDataSource {
+
+    /** The name of the data source. */
+    public name: string;
+    /** The object which specifies HTTP request. */
+    protected sourceRequestObject: ISourceRequest;
+    /** Validation helper */
+    protected validator: Validator;
+    /** Specifies where to look for the unique identifier of the object to find it in the collection. */
+    protected searchPath: string;
+    /** Specifies where is the collection of the individual results stored in the returned object. */
+    protected resultsPath: string;
+
+    constructor() {
+        super();
+        this.name = "RopidGTFSStopsDataSource";
+        this.sourceRequestObject = null;
+        // TODO doplnit validator
+        this.validator = null; // new Validator(this.name, schemaObject);
+        this.resultsPath = "stopGroups";
+        this.searchPath = "";
+    }
+
+    /**
+     * Override
+     */
+    public GetAll = async (): Promise<any> => {
+        const data = await this.GetRawData();
+        if (this.validator) {
+            await this.validator.Validate(data);
+        }
+        return data;
+    }
+
+    /**
+     * Method that connects to remote data endpoint and gets the raw data.
+     *
+     * @returns {Promise<any>} Promise with returned data.
+     */
+    protected GetRawData = async (): Promise<any> => {
+
+        const ftpClient = new ftp.Client();
+        // ftpClient.ftp.verbose = true;
+
+        try {
+            await ftpClient.access(config.datasources.RopidFTP);
+            await ftpClient.cd(config.datasources.RopidGTFSStopsPath);
+            await ftpClient.download(fs.createWriteStream("/tmp/" + config.datasources.RopidGTFSStopsFilename),
+                config.datasources.RopidGTFSStopsFilename);
+
+            const buffer = await this.readFile("/tmp/" + config.datasources.RopidGTFSStopsFilename);
+            const result = Buffer.from(buffer).toString("utf8");
+            return this.GetSubElement(this.resultsPath, JSON.parse(result));
+        } catch (err) {
+            throw new CustomError("Retrieving of the source data failed.", true, this.name, 1002, err);
+        }
+    }
+
+    private readFile = (file: string): Promise<Buffer> => {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file);
+            const chunks = [];
+
+            stream.on("error", (err) => {
+                reject(err);
+            });
+            stream.on("data", (data) => {
+                chunks.push(data);
+            });
+            stream.on("close", () => {
+                resolve(Buffer.concat(chunks));
+            });
+        });
+    }
+
+}
---
+++ src/transformations/RopidGTFSStopsTransformation.ts
@@ -0,0 +1,66 @@
+"use strict";
+
+import { RopidGTFS } from "data-platform-schema-definitions";
+import BaseTransformation from "./BaseTransformation";
+import ITransformation from "./ITransformation";
+
+export default class RopidGTFSStopsTransformation extends BaseTransformation implements ITransformation {
+
+    public name: string;
+
+    constructor() {
+        super();
+        this.name = "RopidGTFSStops"; // RopidGTFS.name;
+    }
+
+    /**
+     * Transforms data from data source to output format (JSON)
+     */
+    public TransformDataElement = async (element): Promise<any> => {
+        // not used
+    }
+
+    /**
+     * Transforms data from data source to output format (JSON)
+     */
+    public TransformDataCollection = async (collection): Promise<any> => {
+        const res = {
+            cis_stop_groups: [],
+            cis_stops: [],
+        };
+
+        collection.map((stopGroup) => {
+            res.cis_stop_groups.push({
+                avgJtskX: stopGroup.avgJtskX,
+                avgJtskY: stopGroup.avgJtskY,
+                avgLat: stopGroup.avgLat,
+                avgLon: stopGroup.avgLon,
+                cis: stopGroup.cis,
+                districtCode: stopGroup.districtCode,
+                fullName: stopGroup.fullName,
+                idosCategory: stopGroup.idosCategory,
+                idosName: stopGroup.idosName,
+                municipality: stopGroup.municipality,
+                name: stopGroup.name,
+                node: stopGroup.node,
+                uniqueName: stopGroup.uniqueName,
+            });
+            stopGroup.stops.map((stop) => {
+                res.cis_stops.push({
+                    altIdosName: stop.altIdosName,
+                    cis: stopGroup.cis,
+                    id: stop.id,
+                    jtskX: stop.jtskX,
+                    jtskY: stop.jtskY,
+                    lat: stop.lat,
+                    lon: stop.lon,
+                    platform: stop.platform,
+                    wheelchairAccess: stop.wheelchairAccess,
+                    zone: stop.zone,
+                });
+            });
+        });
+        return res;
+    }
+
+}
