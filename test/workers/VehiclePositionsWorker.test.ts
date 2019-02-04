dfa591e36ee7b73bc064fda93b6d04185c8c8286
Tomáš Benák <benak@operatorict.cz>
2019-02-04 12:06:34 +0100

    oprava testu

--- test/workers/VehiclePositionsWorker.test.ts
+++ test/workers/VehiclePositionsWorker.test.ts
@@ -18,8 +18,17 @@ describe("VehiclePositionsWorker", () => {
     let worker;
     let sandbox;
     let sequelizeModelStub;
+    let testData;
 
     beforeEach(() => {
+        testData = [{
+            cis_short_name: "999",
+            id: "999",
+            start_cis_stop_id: "999",
+            start_cis_stop_platform_code: "a",
+            start_timestamp: "",
+        }];
+
         sandbox = sinon.createSandbox({ useFakeTimers : true });
         sequelizeModelStub = Object.assign({removeAttribute: sandbox.stub()});
         sandbox.stub(PostgresConnector, "getConnection")
@@ -30,8 +39,12 @@ describe("VehiclePositionsWorker", () => {
             .callsFake(() => Object.assign({ positions: [], stops: [], trips: [] }));
         sandbox.stub(worker.modelPositions, "SaveToDb");
         sandbox.stub(worker.modelStops, "SaveToDb");
-        sandbox.stub(worker.modelTrips, "SaveToDb");
-
+        sandbox.stub(worker.modelTrips, "SaveToDb")
+            .callsFake(() => testData);
+        sandbox.stub(worker.modelTrips, "getTripsWithoutGTFSTripId")
+            .callsFake(() => testData);
+        sandbox.stub(worker.modelTrips, "findAndUpdateGTFSTripId");
+        sandbox.stub(worker, "sendMessageToExchange");
     });
 
     afterEach(() => {
@@ -47,12 +60,28 @@ describe("VehiclePositionsWorker", () => {
         sandbox.assert.calledWith(worker.modelStops.SaveToDb, []);
         sandbox.assert.calledOnce(worker.modelTrips.SaveToDb);
         sandbox.assert.calledWith(worker.modelTrips.SaveToDb, []);
+        sandbox.assert.calledOnce(worker.sendMessageToExchange);
         sandbox.assert.callOrder(
             worker.transformation.TransformDataCollection,
             worker.modelPositions.SaveToDb,
             worker.modelStops.SaveToDb,
-            worker.modelTrips.SaveToDb);
+            worker.modelTrips.SaveToDb,
+            worker.sendMessageToExchange);
         sandbox.assert.calledThrice(PostgresConnector.getConnection);
+    });
+
+    it("should calls the correct methods by getTripsWithoutGTFSTripId method", async () => {
+        await worker.getTripsWithoutGTFSTripId();
+        sandbox.assert.calledOnce(worker.modelTrips.getTripsWithoutGTFSTripId);
+        sandbox.assert.calledOnce(worker.sendMessageToExchange);
+        sandbox.assert.callOrder(
+            worker.modelTrips.getTripsWithoutGTFSTripId);
+        sandbox.assert.calledThrice(PostgresConnector.getConnection);
+    });
+
+    it("should calls the correct methods by updateGTFSTripId method", async () => {
+        await worker.updateGTFSTripId();
+        sandbox.assert.calledOnce(worker.modelTrips.findAndUpdateGTFSTripId);
     });
 
 });
