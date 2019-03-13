d091d8db926d735c06138384dc1ec0854b2eb9df
Tomáš Benák <benak@operatorict.cz>
2019-03-13 16:24:09 +0100

    uprava gitlab-ci, testy pro pripojeni k db

--- test/helpers/RedisConnector.test.ts
+++ test/helpers/RedisConnector.test.ts
@@ -4,7 +4,7 @@
 
 import "mocha";
 import CustomError from "../../src/helpers/errors/CustomError";
-const { PostgresConnector } = require("../../src/helpers/PostgresConnector");
+const { RedisConnector } = require("../../src/helpers/RedisConnector");
 
 const chai = require("chai");
 const expect = chai.expect;
@@ -12,28 +12,28 @@ const chaiAsPromised = require("chai-as-promised");
 
 chai.use(chaiAsPromised);
 
-describe("PostgresConnector", () => {
+describe("RedisConnector", () => {
 
     it("should has connect method", async () => {
-        expect(PostgresConnector.connect).not.to.be.undefined;
+        expect(RedisConnector.connect).not.to.be.undefined;
     });
 
     it("should has getConnection method", async () => {
-        expect(PostgresConnector.getConnection).not.to.be.undefined;
+        expect(RedisConnector.getConnection).not.to.be.undefined;
     });
 
     it("should throws Error if not connect method was not called", () => {
-        expect(PostgresConnector.getConnection).to.throw(CustomError);
+        expect(RedisConnector.getConnection).to.throw(CustomError);
     });
 
     it("should connects to RabbitMQ and returns channel", async () => {
-        const ch = await PostgresConnector.connect();
+        const ch = await RedisConnector.connect();
         expect(ch).to.be.an.instanceof(Object);
     });
 
     it("should returns channel", async () => {
-        await PostgresConnector.connect();
-        expect(PostgresConnector.getConnection()).to.be.an.instanceof(Object);
+        await RedisConnector.connect();
+        expect(RedisConnector.getConnection()).to.be.an.instanceof(Object);
     });
 
 });
