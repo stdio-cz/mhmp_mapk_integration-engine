c6a7cd3de88ad55c0eb3af42709ef97bdb3e1415
Tomáš Benák <benak@operatorict.cz>
2020-01-15 16:28:35 +0100

    create PostgresProtocolStrategy

--- src/core/datasources/IProtocolStrategy.ts
+++ src/core/datasources/IProtocolStrategy.ts
@@ -1,5 +1,7 @@
 "use strict";
 
+import * as Sequelize from "sequelize";
+
 export interface IHTTPSettings {
     /** (optional) Data to send with request, e.g. credentials */
     body?: any;
@@ -42,9 +44,36 @@ export interface IFTPSettings {
     whitelistedFiles?: string[];
 }
 
+export interface IPostgresSettings {
+
+    /** Connection string to PostgreSQL. */
+    connectionString: string;
+
+    /** Schema name. */
+    schemaName: string;
+
+    /** Table name. */
+    tableName: string;
+
+    /** Database table schema. */
+    modelAttributes: Sequelize.DefineModelAttributes<any>;
+
+    /**
+     * Sequelize additional settings, e.g. indexes.
+     * Default values: { freezeTableName: true, timestamps: true, underscored: true }
+     */
+    sequelizeAdditionalSettings?: object;
+
+    /**
+     * Sequelize findAll() options object.
+     * https://sequelize.org/master/class/lib/model.js~Model.html#static-method-findAll
+     */
+    findOptions?: object;
+}
+
 export interface IProtocolStrategy {
 
-    setConnectionSettings(settings: IHTTPSettings | IFTPSettings): void;
+    setConnectionSettings(settings: IHTTPSettings | IFTPSettings | IPostgresSettings): void;
 
     getData(): Promise<any>;
 
