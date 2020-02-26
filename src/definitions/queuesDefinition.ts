cb5199584af422c4cb8bba97deacb9dbd13de388
Tomáš Benák <benak@operatorict.cz>
2020-02-26 15:08:24 +0100

    update schemas to v1.0.0, remove discontinued projects - icegateway, ztp parkings

--- src/definitions/queuesDefinition.ts
+++ src/definitions/queuesDefinition.ts
@@ -2,10 +2,9 @@
 
 import {
     AirQualityStations, BicycleCounters, BicycleParkings, CityDistricts, FirebasePidlitacka, Gardens, GeneralImport,
-    IceGatewaySensors, IceGatewayStreetLamps, MedicalInstitutions, MerakiAccessPoints, Meteosensors, MOS,
-    MunicipalAuthorities, MunicipalPoliceStations, Parkings, ParkingZones, Parkomats, Playgrounds, PublicToilets,
-    RopidGTFS, SharedBikes, SharedCars, SortedWasteStations, TrafficCameras, VehiclePositions, WasteCollectionYards,
-    WazeCCP, ZtpParkings,
+    MedicalInstitutions, MerakiAccessPoints, Meteosensors, MOS, MunicipalAuthorities, MunicipalPoliceStations, Parkings,
+    ParkingZones, Parkomats, Playgrounds, PublicToilets, RopidGTFS, SharedBikes, SharedCars, SortedWasteStations,
+    TrafficCameras, VehiclePositions, WasteCollectionYards, WazeCCP,
 } from "@golemio/schema-definitions";
 import { config } from "../core/config";
 import { IQueueDefinition } from "../core/queueprocessors";
@@ -16,8 +15,6 @@ import { CityDistrictsWorker } from "../modules/citydistricts";
 import { FirebasePidlitackaWorker } from "../modules/firebasepidlitacka";
 import { GardensWorker } from "../modules/gardens";
 import { GeneralWorker } from "../modules/general";
-import { IceGatewaySensorsWorker } from "../modules/icegatewaysensors";
-import { IceGatewayStreetLampsWorker } from "../modules/icegatewaystreetlamps";
 import { MedicalInstitutionsWorker } from "../modules/medicalinstitutions";
 import { MerakiAccessPointsWorker } from "../modules/merakiaccesspoints";
 import { MeteosensorsWorker } from "../modules/meteosensors";
@@ -39,7 +36,6 @@ import { TrafficCamerasWorker } from "../modules/trafficcameras";
 import { VehiclePositionsWorker } from "../modules/vehiclepositions";
 import { WasteCollectionYardsWorker } from "../modules/wastecollectionyards";
 import { WazeCCPWorker } from "../modules/wazeccp";
-import { ZtpParkingsWorker } from "../modules/ztpparkings";
 
 const definitions: IQueueDefinition[] = [
     {
@@ -259,56 +255,6 @@ const definitions: IQueueDefinition[] = [
                 },
                 worker: GardensWorker,
                 workerMethod: "refreshDataInDB",
-            },
-        ],
-    },
-    {
-        name: IceGatewaySensors.name,
-        queuePrefix: config.RABBIT_EXCHANGE_NAME + "." + IceGatewaySensors.name.toLowerCase(),
-        queues: [
-            {
-                name: "refreshDataInDB",
-                options: {
-                    deadLetterExchange: config.RABBIT_EXCHANGE_NAME,
-                    deadLetterRoutingKey: "dead",
-                    messageTtl: 4 * 60 * 1000, // 4 minutes
-                },
-                worker: IceGatewaySensorsWorker,
-                workerMethod: "refreshDataInDB",
-            },
-            {
-                name: "saveDataToHistory",
-                options: {
-                    deadLetterExchange: config.RABBIT_EXCHANGE_NAME,
-                    deadLetterRoutingKey: "dead",
-                },
-                worker: IceGatewaySensorsWorker,
-                workerMethod: "saveDataToHistory",
-            },
-        ],
-    },
-    {
-        name: IceGatewayStreetLamps.name,
-        queuePrefix: config.RABBIT_EXCHANGE_NAME + "." + IceGatewayStreetLamps.name.toLowerCase(),
-        queues: [
-            {
-                name: "refreshDataInDB",
-                options: {
-                    deadLetterExchange: config.RABBIT_EXCHANGE_NAME,
-                    deadLetterRoutingKey: "dead",
-                    messageTtl: 14 * 60 * 1000, // 14 minutes
-                },
-                worker: IceGatewayStreetLampsWorker,
-                workerMethod: "refreshDataInDB",
-            },
-            {
-                name: "setDimValue",
-                options: {
-                    deadLetterExchange: config.RABBIT_EXCHANGE_NAME,
-                    deadLetterRoutingKey: "dead",
-                },
-                worker: IceGatewayStreetLampsWorker,
-                workerMethod: "setDimValue",
             },
         ],
     },
@@ -1061,50 +1007,6 @@ const definitions: IQueueDefinition[] = [
                 },
                 worker: WazeCCPWorker,
                 workerMethod: "refreshJamsInDB",
-            },
-        ],
-    },
-    {
-        name: ZtpParkings.name,
-        queuePrefix: config.RABBIT_EXCHANGE_NAME + "." + ZtpParkings.name.toLowerCase(),
-        queues: [
-            {
-                name: "refreshDataInDB",
-                options: {
-                    deadLetterExchange: config.RABBIT_EXCHANGE_NAME,
-                    deadLetterRoutingKey: "dead",
-                    messageTtl: 59 * 60 * 1000, // 59 minutes
-                },
-                worker: ZtpParkingsWorker,
-                workerMethod: "refreshDataInDB",
-            },
-            {
-                name: "saveDataToHistory",
-                options: {
-                    deadLetterExchange: config.RABBIT_EXCHANGE_NAME,
-                    deadLetterRoutingKey: "dead",
-                },
-                worker: ZtpParkingsWorker,
-                workerMethod: "saveDataToHistory",
-            },
-            {
-                name: "updateAddressAndDistrict",
-                options: {
-                    deadLetterExchange: config.RABBIT_EXCHANGE_NAME,
-                    deadLetterRoutingKey: "dead",
-                    messageTtl: 59 * 60 * 1000, // 59 minutes
-                },
-                worker: ZtpParkingsWorker,
-                workerMethod: "updateAddressAndDistrict",
-            },
-            {
-                name: "updateStatusAndDevice",
-                options: {
-                    deadLetterExchange: config.RABBIT_EXCHANGE_NAME,
-                    deadLetterRoutingKey: "dead",
-                },
-                worker: ZtpParkingsWorker,
-                workerMethod: "updateStatusAndDevice",
             },
         ],
     },
