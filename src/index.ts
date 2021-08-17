9efbc77672d255dcc14acb6b240504bbf2fdf27f
Lenka Cackova <lenka.cacko@gmail.com>
2021-08-17 11:37:17 +0000

    feat: telemetry

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import App from "./App";
+// load telemetry before all deps
+import { initTraceProvider } from "@golemio/core/dist/telemetry";
+initTraceProvider("integration-engine");
 
+// start app
+import App from "./App";
 new App().start();
