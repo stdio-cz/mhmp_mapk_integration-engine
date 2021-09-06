f5e0c18811cc6ee6d527ea041dfc3ce59cc5153f
Lenka Cackova <lenka.cacko@gmail.com>
2021-09-06 11:49:06 +0000

    sentry re-revert

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // load telemetry before all deps
-import { initTraceProvider } from "@golemio/core/dist/telemetry";
+import { initTraceProvider } from "@golemio/core/dist/monitoring";
 import { config } from "@golemio/core/dist/integration-engine/config";
-initTraceProvider("integration-engine", config.NODE_ENV, config.telemetry);
+initTraceProvider(config.app_name, config.NODE_ENV, config.telemetry);
 
 // start app
 import App from "./App";
