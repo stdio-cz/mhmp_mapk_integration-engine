7d05838ffdc97309f42bbbce687052769a06c302
Daniel Knotek <knotekd25@outlook.com>
2021-09-21 17:23:34 +0200

    feat(#246): move some functionality to the core module

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-// load telemetry before all deps
+// Load telemetry before all deps
 import { initTraceProvider } from "@golemio/core/dist/monitoring";
 import { config } from "@golemio/core/dist/integration-engine/config";
 initTraceProvider(config.app_name, config.NODE_ENV, config.telemetry);
 
-// start app
+// Start the app
 import App from "./App";
 new App().start();
