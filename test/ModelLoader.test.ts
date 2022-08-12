ff3dec212e8d9d1c59c54c6f9d1e868ead50f201
Daniel Knotek <knotekd25@outlook.com>
2022-08-12 13:35:45 +0200

    feat: write unit tests for ModuleLoader

---
+++ test/ModelLoader.test.ts
@@ -0,0 +1,32 @@
+import chai, { expect } from "chai";
+import chaiAsPromised from "chai-as-promised";
+import { CustomError } from "@golemio/core/dist/shared/golemio-errors";
+import { AbstractWorker } from "@golemio/core/dist/integration-engine";
+import { ModuleLoader } from "../src/ModuleLoader";
+
+chai.use(chaiAsPromised);
+
+describe("ModuleLoader", () => {
+    // =============================================================================
+    // loadModules
+    // =============================================================================
+    describe("loadModules", () => {
+        it("should load queue definitions and workers", async () => {
+            ModuleLoader["modules"] = ["playgrounds"];
+
+            const result = await ModuleLoader.loadModules();
+            expect(result.queueDefinitions).to.deep.equal([]);
+            expect(new result.workers[0]()).to.be.instanceOf(AbstractWorker);
+        });
+
+        it("should reject (non-existent module)", async () => {
+            ModuleLoader["modules"] = ["fus-ro-dah"];
+
+            const promise = ModuleLoader.loadModules();
+            await expect(promise).to.be.rejectedWith(
+                CustomError,
+                "Cannot import module @golemio/fus-ro-dah/dist/integration-engine"
+            );
+        });
+    });
+});
